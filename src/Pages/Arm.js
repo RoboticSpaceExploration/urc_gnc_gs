63daf383e3acea4864a62a0cb7c536406b1eb1a0
joshrico <jrico@hawaii.edu>
2022-10-14 18:09:29 -1000

    Initial design implemented for the Arm Page

--- src/Pages/Arm.js
+++ src/Pages/Arm.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Grid, Header } from 'semantic-ui-react';
 
 class Arm extends React.Component {
   render() {
-          return (
-                  <div id="arm-page">
-                    <h1>THIS IS ARM</h1>
-                  </div>
-        );
+    return (
+      <Grid Container centered id="arm-page" columns={2}>
+        <Grid.Row>
+          <Header as="h1" textAlign="center">ARM</Header>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+	    <Header as="h2" textAlign="center">Cam 1</Header>
+          </Grid.Column>
+	  <Grid.Column>
+	    <Header as="h2" textAlign="center">Cam 2</Header>
+	  </Grid.Column>
+	</Grid.Row>
+        <Grid.Row>
+	  <Grid.Column>
+	    <Header as="h2" textAlign="center">WASD 1</Header>
+	  </Grid.Column>
+	  <Grid.Column>
+	    <Header as="h2" textAlign="center">WASD 2</Header>
+	  </Grid.Column>
+	</Grid.Row>
+      </Grid>
+    );
   };
 }
 
