132264f8da5ec28ea3c034ccbf304ba8b6aa9cf9
jgaleria <jgaleria@hawaii.edu>
2022-11-15 16:05:29 -1000

    Testing robot home control page

---
+++ src/Pages/Home.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import {Button} from "semantic-ui-react";
+
+class Home extends Component {
+    state = {
+        counter: 1,
+    };
+
+    increment_counter() {
+        //this.state.counter = this.state.counter +1;
+        this.setState({ counter: this.state.counter + 1});
+        console.log(this.state.counter);
+    }
+
+    render() {
+        return (
+            <main>
+                <h1 className = 'text-center mt-3'> Robot Control Page</h1>
+                <h5> Counter: <span> {this.state.counter}</span>
+                </h5>
+
+                {/* <Button> Increment </Button> */}
+                <Button onClick= {()=> this.increment_counter}> Increment</Button>
+            </main>
+        );
+    }
+}
+
+export default Home;
