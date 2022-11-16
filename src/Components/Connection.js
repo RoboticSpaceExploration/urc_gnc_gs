757d673dcc2c77e70e4bfe73f03ed1994d579a76
jgaleria <jgaleria@hawaii.edu>
2022-11-15 18:19:36 -1000

    Making connection to robot

--- src/Components/Connection.js
+++ src/Components/Connection.js
@@ -1,15 +1,51 @@
 import React, {Component} from 'react';
+import { Message } from 'semantic-ui-react';
 
 class Connection extends Component {
-    state = {}
+    state = { connected: false, ros: null};
+
+    constructor() {
+        super();
+        this.init_connection();
+    }
 
     init_connection(){
-        var ros = new window.ROSLIB.Ros();
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+
+        this.state.ros.on("connection", ()=> {
+            console.log("connection established");
+            this.setState({connected: true }); 
+        });
+
+        this.state.ros.on("close", ()=> {
+            console.log("connection is closed");
+            this.setState({connected: false });
+            //try to reconnect every 3 seconds
+            setTimeout(()=> {
+                try {
+                    this.state.ros.connect("ws://192.168.1.145:9090");
+                }
+                catch (error ){
+                    console.log("connection problem");
+                }
+            }, 3000);
+
+        });
+
+        try {
+            this.state.ros.connect("ws://192.168.1.145:9090");
+        }
+        catch (error ){
+            console.log("connection problem");
+        }
     }
 
     render() {
         return (
-            <h2>Connection</h2>
+            <Message className="text-center m-3"
+            variant={this.state.connected? "success": "danger"}>
+                {this.state.connected? "Robot Connected": "Robot Disconnected"}</Message>
         )
     }
 }
