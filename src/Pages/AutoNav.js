9b45befde61b423ffb44c9a313afb67c489ce82d
joshrico <jrico@hawaii.edu>
2023-01-25 17:25:28 -1000

    Added Client Side Data Validation
    - prevents empty field submission
    - shows error upon trying

--- src/Pages/AutoNav.js
+++ src/Pages/AutoNav.js
@@ -15,29 +15,30 @@ import ArmLogo from '../Images/arm.png';
 import Connection from '../Components/Connection';
 
 function AutoNav() {
-  const [ waypointData, setWaypointData] = useState(null);
-  const [ newCoord, setCoord ] = useState({
-      longitude: null,
-      latitude: null
-  });
+    const [ coordValidated, setCoordValidated ] = useState(false);
+    const [ waypointData, setWaypointData] = useState(null);
+    const [ newCoord, setCoord ] = useState({
+        longitude: null,
+        latitude: null
+    });
 
-  const cardStyle = { height: '70vh' };
-  const titleStyle = { textAlign: 'center', marginBottom: '10px' };
+    const cardStyle = { height: '70vh' };
+    const titleStyle = { textAlign: 'center', marginBottom: '10px' };
 
-  useEffect(() => {
-    axios({
-	    method: "GET",
-	    url:"http://localhost:9000/waypoints",
-    }).then((response) => {
-	    const res = response.data;
-	    setWaypointData(res);
-    }).catch((error) => {
-	    if (error.response) {
-		    console.log(error.response);
-		    console.log(error.response.status);
-		    console.log(error.response.headers);
-	    }
-    })},[])
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url:"http://localhost:9000/waypoints",
+        }).then((response) => {
+            const res = response.data;
+            setWaypointData(res);
+        }).catch((error) => {
+            if (error.response) {
+                console.log(error.response);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+        })},[])
 
     const handleChange = (e) => {
         const value = e.target.value;
@@ -48,14 +49,18 @@ function AutoNav() {
     };
 
     const handleSubmit = (e) => {
+        const form = e.currentTarget;
         e.preventDefault();
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+
+        setCoordValidated(true);
         const coordData = {
             longitude: newCoord.longitude,
             latitude: newCoord.latitude
         };
-        if (!coordData.longitude || !coordData.latitude){
-            console.log("xd");
-        } else {
+        if (coordData.longitude && coordData.latitude) {
             axios.post("http://localhost:9000/autonav", coordData).then((response) => {
                 console.log(response.status);
                 console.log(response.data.token);
@@ -66,75 +71,81 @@ function AutoNav() {
 
     return (
         <Container style={{ marginTop: '10px' }}>
-          <Row style={{ textAlign: 'center', display: 'flex', flexWrap: 'wrap' }}>
-            <Col style={{ alignSelf: 'center' }} xs={10}>
-              <h1>AutoNav</h1>
-            </Col>
-
-            <Col style={{ alignSelf: 'center' }} xs={2}>
-              <Connection />
-            </Col>
-          </Row>
-
-          <Card style={cardStyle}>
-            <div>
-              <Row style={{ display: 'flex' }}>
-                <Col style={cardStyle} className="divider" xs={4}>
-                  <h3 style={{ textAlign: 'center' }}>Queue List</h3>
-                  <div>
-                    <Toast>
-                      <Toast.Header closeButton={false}>
-                        <img
-                            src={RoseLogo}
-                            className="queue-img"
-                            alt=""
-                        />
-                        <strong className="me-auto">1</strong>
-                        <small className="text-muted">just now</small>
-                      </Toast.Header>
-                      <Toast.Body>Rover did thing 1</Toast.Body>
-                    </Toast>
-                    <Toast>
-                      <Toast.Header closeButton={false}>
-                        <img
-                            src={AutoNavLogo}
-                            className="queue-img"
-                            alt=""
-                        />
-                        <strong className="me-auto">2</strong>
-                        <small className="text-muted">just now</small>
-                      </Toast.Header>
-                      <Toast.Body>Rover did thing 2</Toast.Body>
-                    </Toast>
-                  </div>
-
+            <Row style={{ textAlign: 'center', display: 'flex', flexWrap: 'wrap' }}>
+                <Col style={{ alignSelf: 'center' }} xs={10}>
+                    <h1>AutoNav</h1>
                 </Col>
 
-                <Col style={{ height: '60vh' }} >
-                  <h3 style={{ textAlign: 'center' }}>Map</h3>
-                  {waypointData &&
-                      waypointData.map((waypoint, index) => {
-                        console.log(waypoint);
-                        return (
+                <Col style={{ alignSelf: 'center' }} xs={2}>
+                    <Connection />
+                </Col>
+            </Row>
+
+            <Card style={cardStyle}>
+                <div>
+                    <Row style={{ display: 'flex' }}>
+                        <Col style={cardStyle} className="divider" xs={4}>
+                            <h3 style={{ textAlign: 'center' }}>Queue List</h3>
                             <div>
-                              <h1 style={{ textAlign: "center" }}>id: {waypoint.id}</h1>
-                              <h1 style={{ textAlign: "center" }}>type: {waypoint.type}</h1>
-                              <h1 style={{ textAlign: "center" }}>latitude: {waypoint.latitude}</h1>
-                              <h1 style={{ textAlign: "center" }}>longitude: {waypoint.longitude}</h1>
-                              <h1 style={{ textAlign: "center" }}>visited: {waypoint.visited}</h1>
-                              <h1 style={{ textAlign: "center" }}>visible: {waypoint.visible}</h1>
+                                <Toast>
+                                    <Toast.Header closeButton={false}>
+                                        <img
+                                            src={RoseLogo}
+                                            className="queue-img"
+                                            alt=""
+                                        />
+                                        <strong className="me-auto">1</strong>
+                                        <small className="text-muted">just now</small>
+                                    </Toast.Header>
+                                    <Toast.Body>Rover did thing 1</Toast.Body>
+                                </Toast>
+                                <Toast>
+                                    <Toast.Header closeButton={false}>
+                                        <img
+                                            src={AutoNavLogo}
+                                            className="queue-img"
+                                            alt=""
+                                        />
+                                        <strong className="me-auto">2</strong>
+                                        <small className="text-muted">just now</small>
+                                    </Toast.Header>
+                                    <Toast.Body>Rover did thing 2</Toast.Body>
+                                </Toast>
                             </div>
-                        );
-                      })
-                  }
-                  <Form onSubmit={handleSubmit}>
-                    <Form.Control type="number" step="0.01" name="longitude" placeholder="enter longitude" onChange={handleChange}/>
-                    <Form.Control type="number" step="0.01" name="latitude" placeholder="enter latitude" onChange={handleChange}/>
-                    <Button type="submit" >Submit</Button>
-                  </Form>
-                </Col>
-              </Row>
-            </div>
+
+                        </Col>
+
+                        <Col style={{ height: '60vh' }} >
+                            <h3 style={{ textAlign: 'center' }}>Map</h3>
+                            {waypointData &&
+                                waypointData.map((waypoint, index) => {
+                                    console.log(waypoint);
+                                    return (
+                                        <div>
+                                            <h1 style={{ textAlign: "center" }}>id: {waypoint.id}</h1>
+                                            <h1 style={{ textAlign: "center" }}>type: {waypoint.type}</h1>
+                                            <h1 style={{ textAlign: "center" }}>latitude: {waypoint.latitude}</h1>
+                                            <h1 style={{ textAlign: "center" }}>longitude: {waypoint.longitude}</h1>
+                                            <h1 style={{ textAlign: "center" }}>visited: {waypoint.visited}</h1>
+                                            <h1 style={{ textAlign: "center" }}>visible: {waypoint.visible}</h1>
+                                        </div>
+                                    );
+                                })
+                            }
+                            <Form noValidate validated={coordValidated} onSubmit={handleSubmit}>
+                                <Form.Control type="number" step="0.01" name="longitude" placeholder="enter longitude" onChange={handleChange} required/>
+                                <Form.Control.Feedback type="invalid">
+                                    Please valid input.
+                                </Form.Control.Feedback>
+                                <Form.Control type="number" step="0.01" name="latitude" placeholder="enter latitude" onChange={handleChange} required/>
+                                <Form.Control.Feedback type="invalid">
+                                    Please valid input.
+                                </Form.Control.Feedback>
+                                <Button type="submit" >Submit</Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </div>
             </Card>
         </Container>
 
--- src/Pages/Payload.js
+++ src/Pages/Payload.js
@@ -4,49 +4,60 @@ import Form from 'react-bootstrap/Form';
 import axios from "axios";
 
 function Payload() {
-  const [payloadData, setPayloadData] = useState(null);
-  const [newData, setData] = useState({
-    data: ""
-  });
-  const [newTemp, setTemp] = useState({
-      temperature: null
-  });
-
-  useEffect(() => {
-    axios({
-	    method: "GET",
-	    url:"http://localhost:9000/payload",
-    }).then((response) => {
-	    const res = response.data;
-	    setPayloadData(res);
-    }).catch((error) => {
-	    if (error.response) {
-		    console.log(error.response);
-		    console.log(error.response.status);
-		    console.log(error.response.headers);
-	    }
-    })},[])
-
-  const dataHandleChange = (e) => {
-    const value = e.target.value;
-    setData({
-      ...newData,
-      [e.target.name]: value
+    const [ payloadValidated, setPayloadValidated ] = useState(false);
+    const [ tempValidated, setTempValidated ] = useState(false);
+    const [payloadData, setPayloadData] = useState(null);
+    const [newData, setData] = useState({
+        data: ""
     });
-  };
+    const [newTemp, setTemp] = useState({
+        temperature: null
+    });
 
-  const dataHandleSubmit = (e) => {
-    e.preventDefault();
-    const payData = {
-      id: newData.id,
-      data: newData.data
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url:"http://localhost:9000/payload",
+        }).then((response) => {
+            const res = response.data;
+            setPayloadData(res);
+        }).catch((error) => {
+            if (error.response) {
+                console.log(error.response);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+        })},[])
+
+    const dataHandleChange = (e) => {
+        const value = e.target.value;
+        setData({
+            ...newData,
+            [e.target.name]: value
+        });
     };
-    axios.post("http://localhost:9000/payload", payData).then((response) => {
-      console.log(response.status);
-      console.log(response.data.token);
-      window.location.reload();
-    });
-  };
+
+    const dataHandleSubmit = (e) => {
+        const form = e.currentTarget;
+        e.preventDefault();
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+
+        setPayloadValidated(true);
+        const payData = {
+            id: newData.id,
+            data: newData.data
+        };
+        if (payData.data){
+            axios.post("http://localhost:9000/payload", payData).then((response) => {
+                console.log(response.status);
+                console.log(response.data.token);
+                window.location.reload();
+            });
+        }
+
+    };
 
     const tempHandleChange = (e) => {
         const value = e.target.value;
@@ -57,42 +68,55 @@ function Payload() {
     };
 
     const tempHandleSubmit = (e) => {
+        const form = e.currentTarget;
         e.preventDefault();
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+
+        setTempValidated(true);
         const tempData = {
-            id: newTemp.id,
             temperature: newTemp.temperature
         };
-        axios.post("http://localhost:9000/temp", tempData).then((response) => {
-            console.log(response.status);
-            console.log(response.data.token);
-            window.location.reload();
-        });
+        if (tempData.temperature) {
+            axios.post("http://localhost:9000/temp", tempData).then((response) => {
+                console.log(response.status);
+                console.log(response.data.token);
+                window.location.reload();
+            });
+        }
     };
 
-  return (
-    <div id="payload-page">
-      <h1>PAYLOAD</h1>
-	  {payloadData &&
-          payloadData.map((data, index) => {
-              console.log(data);
-              return (
-                  <div>
-                      <h2>id: {data.id}</h2>
-                      <h2>data: {data.data}</h2>
-                  </div>
-              );
-          })
-      }
-	  <Form onSubmit={dataHandleSubmit}>
-          <Form.Control type="text" name="data" placeholder="enter data" onChange={dataHandleChange}/>
-        <Button type="submit" variant="secondary">Submit</Button>
-      </Form>
-      <Form onSubmit={tempHandleSubmit}>
-          <Form.Control type="number" step="0.01" name="temperature" placeholder="enter temperature" onChange={tempHandleChange}/>
-          <Button type="submit" variant="secondary">Submit</Button>
-      </Form>
-    </div>
-  );
+    return (
+        <div id="payload-page">
+            <h1>PAYLOAD</h1>
+            {payloadData &&
+                payloadData.map((data, index) => {
+                    console.log(data);
+                    return (
+                        <div>
+                            <h2>id: {data.id}</h2>
+                            <h2>data: {data.data}</h2>
+                        </div>
+                    );
+                })
+            }
+            <Form noValidate validated={payloadValidated} onSubmit={dataHandleSubmit}>
+                <Form.Control type="text" name="data" placeholder="enter data" onChange={dataHandleChange} required/>
+                <Form.Control.Feedback type="invalid">
+                    Please provide data.
+                </Form.Control.Feedback>
+                <Button type="submit" variant="secondary">Submit</Button>
+            </Form>
+            <Form noValidate validated={tempValidated} onSubmit={tempHandleSubmit}>
+                <Form.Control type="number" step="0.01" name="temperature" placeholder="enter temperature" onChange={tempHandleChange} required/>
+                <Form.Control.Feedback type="invalid">
+                    Please valid input.
+                </Form.Control.Feedback>
+                <Button type="submit" variant="secondary">Submit</Button>
+            </Form>
+        </div>
+    );
 }
 
 export default Payload;
