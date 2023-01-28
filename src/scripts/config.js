25cfa8944a7d100a9cb6d6759fc732112748bfd6
jgaleria <jgaleria@hawaii.edu>
2023-01-28 12:08:28 -1000

    Change port number

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,6 +1,6 @@
 const Config = {
     ROSBRIDGE_SERVER_IP: "192.168.0.223",
-    ROSBRIDGE_SERVER_PORT: "8080",
+    ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIMER: 3000,
     CMD_VEL_TOPIC: "/turtle1/cmd_vel"
 };
