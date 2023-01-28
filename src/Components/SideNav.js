6eaeda2542fba991b9694679ecdc73dee7bc1461
Tsz Ching Wong <tsz_wong@datahouse.com>
2023-01-27 19:17:31 -1000

    added key prop to every element being mapped.

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Image from 'react-bootstrap/Image';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
-import RoseLogo from '../Images/rose-logo.png';
-import AutoNavLogo from '../Images/autonav.png';
-import RoverDataLogo from '../Images/rover-data.png';
-import PayloadLogo from '../Images/payload.png';
-import ArmLogo from '../Images/arm.png';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Tab from "react-bootstrap/Tab";
+import RoseLogo from "../Images/rose-logo.png";
+import AutoNavLogo from "../Images/autonav.png";
+import RoverDataLogo from "../Images/rover-data.png";
+import PayloadLogo from "../Images/payload.png";
+import ArmLogo from "../Images/arm.png";
 
 const SideNav = (props) => {
   return (
@@ -18,20 +18,55 @@ const SideNav = (props) => {
         <Row>
           <Col>
             <Nav variant="pills" className="flex-column">
-              <Nav.Item eventKey="first" id="navbar-rose-logo" as={NavLink} activeClassName="active" exact to="/home">
-                  <Image src={RoseLogo} className="sidenav-logos" />
+              <Nav.Item
+                key="first"
+                id="navbar-rose-logo"
+                as={NavLink}
+                className="active"
+                exact="true"
+                to="/home"
+              >
+                <Image src={RoseLogo} className="sidenav-logos" />
               </Nav.Item>
-              <Nav.Item eventKey="second" id="navbar-rover-data" as={NavLink} activeClassName="active" exact to="/roverdata">
-                  <Image src={RoverDataLogo} className="sidenav-logos" />
+              <Nav.Item
+                key="second"
+                id="navbar-rover-data"
+                as={NavLink}
+                className="active"
+                exact="true"
+                to="/roverdata"
+              >
+                <Image src={RoverDataLogo} className="sidenav-logos" />
               </Nav.Item>
-              <Nav.Item eventKey="third" id="navbar-autonav" as={NavLink} activeClassName="active" exact to="/autonav">
-                  <Image src={AutoNavLogo} className="sidenav-logos" />
+              <Nav.Item
+                key="third"
+                id="navbar-autonav"
+                as={NavLink}
+                className="active"
+                exact="true"
+                to="/autonav"
+              >
+                <Image src={AutoNavLogo} className="sidenav-logos" />
               </Nav.Item>
-              <Nav.Item eventKey="fourth" id="navbar-payload" as={NavLink} activeClassName="active" exact to="/payload">
-                  <Image src={PayloadLogo} className="sidenav-logos" />
+              <Nav.Item
+                key="fourth"
+                id="navbar-payload"
+                as={NavLink}
+                className="active"
+                exact="true"
+                to="/payload"
+              >
+                <Image src={PayloadLogo} className="sidenav-logos" />
               </Nav.Item>
-              <Nav.Item eventKey="fifth" id="navbar-arm" as={NavLink} activeClassName="active" exact to="/arm">
-                  <Image src={ArmLogo} className="sidenav-logos" />
+              <Nav.Item
+                key="fifth"
+                id="navbar-arm"
+                as={NavLink}
+                className="active"
+                exact="true"
+                to="/arm"
+              >
+                <Image src={ArmLogo} className="sidenav-logos" />
               </Nav.Item>
             </Nav>
           </Col>
