d3851334dab3a80fe10578f17532cf15fed7f567
jgaleria <jgaleria@hawaii.edu>
2023-01-28 19:02:13 -1000

    Camera Topic

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,8 +1,10 @@
 const Config = {
     ROSBRIDGE_SERVER_IP: "192.168.0.223",
+
     ROSBRIDGE_SERVER_PORT: "9090",
     RECONNECTION_TIMER: 3000,
-    CMD_VEL_TOPIC: "/turtle1/cmd_vel"
+    CMD_VEL_TOPIC: "/turtle1/cmd_vel",
+    CMD_CAM_TOPIC: "/d435i/color/image_raw/compressed"
 };
 
 export default Config;
