252a45379eb5eb9d4eeae3d27e96e38319035036
Joshua Rico <jrico@hawaii.edu>
2023-02-10 19:06:19 -1000

    Dockerfile Checkpoint
    - able to get Dockerfile to work
    - updated react-scripts to 5.0.1

---
+++ build/precache-manifest.32fa0e882bd2d0f4c7e57b6ab8b399e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e0e0c36a4cfd59cefe",
+    "url": "/static/css/main.bb8a98df.chunk.css"
+  },
+  {
+    "revision": "82e0e0c36a4cfd59cefe",
+    "url": "/static/js/main.11d2318a.chunk.js"
+  },
+  {
+    "revision": "881cdfd7ecd5d3f71928",
+    "url": "/static/js/runtime~main.789632c9.js"
+  },
+  {
+    "revision": "1a8a73306316ae6ffffc",
+    "url": "/static/css/2.38438801.chunk.css"
+  },
+  {
+    "revision": "1a8a73306316ae6ffffc",
+    "url": "/static/js/2.3d092f88.chunk.js"
+  },
+  {
+    "revision": "f5c37e184c05f8ddf122",
+    "url": "/static/js/3.ec39fc1c.chunk.js"
+  },
+  {
+    "revision": "44327bac606214aa40d34086785771de",
+    "url": "/static/media/rose-logo.44327bac.png"
+  },
+  {
+    "revision": "7f1b74806db9f5e80d8b21608f2810f6",
+    "url": "/static/media/autonav.7f1b7480.png"
+  },
+  {
+    "revision": "2c9afe537d230ef8c882be1ce319e52d",
+    "url": "/static/media/rover-data.2c9afe53.png"
+  },
+  {
+    "revision": "49729f103d1f9501f76c26e08682c724",
+    "url": "/static/media/payload.49729f10.png"
+  },
+  {
+    "revision": "ceb791f44bd449e04814b95a29d4e571",
+    "url": "/static/media/arm.ceb791f4.png"
+  },
+  {
+    "revision": "cee9740fef09d6e941daa7750f0ab6ec",
+    "url": "/index.html"
+  }
+];
