5bb7a623c78d4f983aaf9c2ebd3d6af353e82036
Joshua Rico <jrico@hawaii.edu>
2023-02-16 17:04:01 -1000

    Created init_ros_connection
    - created ROSConnection component for init_ros_connection
    - - Consolidated rosbridge related items
    - refactored the source code to utilize init_ros_connection instead of Config script

--- src/Components/Connection.js
+++ src/Components/Connection.js
@@ -1,65 +1,14 @@
 import React, {Component} from 'react';
 import Alert from 'react-bootstrap/Alert';
-import Config from "../scripts/config";
+import { init_ros_connection } from '../ROSConnection';
 
 class Connection extends Component {
-    state = { connected: false, ros: null};
-
-    constructor() {
-        super();
-        this.init_connection();
-    }
-
-    init_connection(){
-        this.state.ros = new window.ROSLIB.Ros();
-        console.log(this.state.ros);
-
-        this.state.ros.on("connection", ()=> {
-            console.log("connection established");
-            this.setState({connected: true }); 
-        });
-
-        this.state.ros.on("close", ()=> {
-            console.log("connection is closed");
-            this.setState({connected: false });
-            //try to reconnect every 3 seconds
-            setTimeout(()=> {
-                try {
-                    this.state.ros.connect(
-                        "ws://"+
-                        Config.ROSBRIDGE_SERVER_IP+
-                        ":"+
-                        Config.ROSBRIDGE_SERVER_PORT+
-                        ""
-                    );
-                }
-                catch (error ){
-                    console.log("connection problem");
-                }
-            }, Config.RECONNECTION_TIMER);
-
-        });
-
-        try {
-            this.state.ros.connect(
-                "ws://"+
-                Config.ROSBRIDGE_SERVER_IP+
-                ":"+
-                Config.ROSBRIDGE_SERVER_PORT+
-                ""
-            );
-        }
-        catch (error ){
-            console.log("connection problem");
-        }
-    }
-
     render() {
         return (
             <Alert className="text-center"
-            variant={this.state.connected ? 'success': 'danger'}>
+            variant={init_ros_connection.connected ? 'success': 'danger'}>
               <h5>
-                {this.state.connected ? "Robot Connected": "Robot Disconnected"}
+                {init_ros_connection.connected ? "Robot Connected": "Robot Disconnected"}
               </h5>
             </Alert>
         )
