fc1dbb3e90e1151558007a5a84b19b530fe78508
pmmanuel315 <pmmanuel315@gmail.com>
2023-10-18 10:40:15 -1000

    Update GPSCoordinates.js

--- frontend/src/Components/GPSCoordinates.js
+++ frontend/src/Components/GPSCoordinates.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Alert } from 'react-bootstrap';
 
 function GPSCoordinates(props) {
     return (
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
               <p><strong>Longitude:  </strong>[longitude goes here]</p>
               <p><strong>Latitude: </strong>[latitude goes here]</p>
           </div>
