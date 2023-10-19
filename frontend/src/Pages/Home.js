85f2b9d0be0f623de33387c50c0101c2162fc49b
Leighton Miguel <leim56@hawaii.rr.com>
2023-10-18 22:21:46 -1000

    implemented close window

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -7,99 +7,53 @@ import Payload from "./Payload";
 import AutoNav from "./AutoNav";
 import System from "./System";
 
-const  Home = ()=>{
+const Home = ()=>{
 
-        
+        useEffect(()=>{
 
-          useEffect(()=>{
+          document.getElementById('navbar-autonav').onclick = ()=>{
 
-            document.getElementById('navbar-autonav').onclick = ()=>{
-
-              if(windowActive.autonav==false){
-                changeWindowArray([...windowArray, 
-                  <div id="window-autonav" style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
-                    <Card>
-                    
-                        {/* <i onclick={window.alert("hey")} id='close-autonav' className="fa-solid fa-x" style={{padding:10,}} /> */}
-                        
-                
-                      <AutoNav/>
-                    </Card>
-                
-                  </div>
-                ])
-                changeWindowActive({...windowActive,autonav:true})
-              }
-              else{
-                document.getElementById("window-autonav").scrollIntoView()
-              }
+            if(windowActive.autonav===false){
+              changeWindowArray([...windowArray,'autonav'])
+              changeWindowActive({...windowActive,autonav:true})
             }
-
-            document.getElementById('navbar-payload').onclick = ()=>{
-              if(windowActive.payload==false){
-                changeWindowArray([...windowArray, 
-                  <div id="window-payload" style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
-                    <Card>
-                    
-                        {/* <i onclick={window.alert("hey")} id='close-autonav' className="fa-solid fa-x" style={{padding:10,}} /> */}
-                        
-                
-                      <Payload/>
-                    </Card>
-                
-                  </div>
-                ])
-                changeWindowActive({...windowActive,payload:true})
-              }
-              else{
-                document.getElementById("window-payload").scrollIntoView()
-              }
+            else{
+              document.getElementById("window-autonav").scrollIntoView()
             }
+          }
 
-            document.getElementById('navbar-arm').onclick = ()=>{
-              if(windowActive.arm==false){
-                changeWindowArray([...windowArray, 
-                  <div id="window-arm" style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
-                    <Card>
-                    
-                        {/* <i onclick={window.alert("hey")} id='close-autonav' className="fa-solid fa-x" style={{padding:10,}} /> */}
-                        
-                
-                      <Arm/>
-                    </Card>
-                
-                  </div>
-                ])
-                changeWindowActive({...windowActive,arm:true})
-              }
-              else{
-                document.getElementById("window-arm").scrollIntoView()
-              }
+          document.getElementById('navbar-payload').onclick = ()=>{
+            if(windowActive.payload===false){
+              changeWindowArray([...windowArray,'payload'])
+              changeWindowActive({...windowActive,payload:true})
             }
-
-            document.getElementById('navbar-system').onclick = ()=>{
-              if(windowActive.system==false){
-                changeWindowArray([...windowArray, 
-                  <div id="window-system"style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
-                    <Card>
-                    
-                        {/* <i onclick={window.alert("hey")} id='close-autonav' className="fa-solid fa-x" style={{padding:10,}} /> */}
-                        
-                
-                      <System/>
-                    </Card>
-                
-                  </div>
-                ])
-                changeWindowActive({...windowActive,system:true})
-              }
-              else{
-                document.getElementById("window-system").scrollIntoView()
-              }
+            else{
+              document.getElementById("window-payload").scrollIntoView()
             }
+          }
+
+          document.getElementById('navbar-arm').onclick = ()=>{
+            if(windowActive.arm===false){
+              changeWindowArray([...windowArray,'arm'])
+              changeWindowActive({...windowActive,arm:true})
+            }
+            else{
+              document.getElementById("window-arm").scrollIntoView()
+            }
+          }
+
+          document.getElementById('navbar-system').onclick = ()=>{
+            if(windowActive.system===false){
+              changeWindowArray([...windowArray,'system'])
+              changeWindowActive({...windowActive,system:true})
+            }
+            else{
+              document.getElementById("window-system").scrollIntoView()
+            }
+          }
 
 
-          })
+        })
 
           const [windowArray,changeWindowArray] = useState([])
           const [windowActive,changeWindowActive] = useState({
@@ -110,13 +64,99 @@ const  Home = ()=>{
             system:false,
 
           })
+
+
+          const handleClose = (input) =>{
+            const newArray = []
+           for(let i=0;i<windowArray.length;i++){
+            if(windowArray[i]!==input){
+              newArray.push(windowArray[i])
+            }
+           }
+          changeWindowArray(newArray)
+          changeWindowActive({...windowActive,[input]:false})
+          }
+
           
 
+          const renderWindows = () =>{
+            const ret = []
 
-         
+            windowArray.forEach((window)=>{
+
+              switch(window){
+
+                case "autonav":
+                  ret.push(
+                  <div id="window-autonav"style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
+                    <Card>
+                      <Button 
+                      onClick={()=>{
+                        handleClose('autonav')
+                        
+                      }}
+                      > X </Button>
+                      <AutoNav/>
+                    </Card>
+                  </div>)
+                  break
+                
+                case "payload":
+                  ret.push(
+                  <div id="window-payload"style={{ paddingTop: '20px', paddingBottom: '20px',width:'47%',}}>
+                    <Card> 
+                    <Button 
+                      onClick={()=>{
+                        handleClose('payload')
+                        
+                      }}
+                      > X </Button>
+                      <Payload/>
+                    </Card>
+                  </div>)
+                  break
+
+                case "arm":
+                  ret.push(
+                  <div id="window-arm"style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
+                    <Card>
+                    <Button 
+                      onClick={()=>{
+                        handleClose('arm')
+                        
+                      }}
+                      > X </Button>
+                      <Arm/>
+                    </Card>
+                  </div>)
+                  break
+
+                case "system":
+                  ret.push(
+                  <div id="window-system"style={{ paddingTop: '20px', paddingBottom: '20px',width:'48%',}}>
+                    <Card>
+                    <Button 
+                      onClick={()=>{
+                        handleClose('system')
+                        
+                      }}
+                      > X </Button>
+                      <System/>
+                    </Card>
+                  </div>)
+                  break
+
+                default:
+                  
 
 
-          
+              }
+
+            })
+
+            return ret
+
+          }
 
           return(
           <div id="home-page">
@@ -130,36 +170,15 @@ const  Home = ()=>{
               <Col lg={10} >
              
 
-                  
                 <h1 className = 'text-center mt-3'> Robot Control Page</h1>
 
                 <Connection />
 
                 <div className=" d-flex  flex-row flex-wrap justify-content-between">
                
-
-                   {/* <div style={{ paddingTop: '20px', paddingBottom: '20px',width:'50%',padding:10}}>
-
-                    <Card>
-                      <Arm/>
-                    </Card>
-                  </div>
-
-                  <div style={{ paddingTop: '20px', paddingBottom: '20px',width:'50%',padding:10}}>
-
-                    <Card>
-                      <Arm/>
-                    </Card>
-                  </div>  */}
-
                   
-                 
-                 {windowArray}
+                  {renderWindows()}
 
-                 
-
-                  
-                  
 
                 </div>
 
