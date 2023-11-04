15f0f3c65f2b4e0252b8a520975bba92322557e7
byilagan808 <b808ilagan@gmail.com>
2023-11-03 14:57:36 -1000

    Alert multiple indicators

--- frontend/src/Components/Connection.js
+++ frontend/src/Components/Connection.js
@@ -3,16 +3,44 @@ import { Alert } from 'react-bootstrap';
 import { init_ros_connection } from '../ROSConnection';
 
 class Connection extends React.Component {
-    render() {
-        return (
-            <Alert className="text-center"
-            variant={init_ros_connection.connected ? 'success': 'danger'}>
-              <h5>
-                {init_ros_connection.connected ? "Robot Connected": "Robot Disconnected"}
-              </h5>
-            </Alert>
-        )
-    }
-}
 
+  render() {
+
+    const armMoving = true;
+    const roverMoving = false;
+
+    let message = '';
+    let variant ='';
+
+    if(init_ros_connection.connected) {
+      if(armMoving){
+        message = 'Rover Arm Moving'
+        variant = 'primary'
+      }
+      else if (roverMoving) {
+        message = 'Rover is Moving'
+        variant = 'info'
+      }
+      else {
+        message = 'Robot Connected'
+        variant = 'success'
+      }
+    }
+    else{
+      message = 'Robot Disconnected'
+      variant = 'danger'
+    }
+
+      return (
+        
+        <div style={{ display:'flex', justifyContent:'center',flexDirection:'row'}}>
+          <Alert className="text-center" style={{ width: '100%' }} variant={variant}>
+              <h5>
+               {message}
+              </h5>
+              </Alert>     
+            </div>
+        )
+      }
+    }
 export default Connection;
