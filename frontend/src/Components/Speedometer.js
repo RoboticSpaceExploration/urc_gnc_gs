93ddf2e3e9e268cc6263e23ae283882626ce42bd
jalenlum <808jalen@gmail.com>
2023-11-28 23:36:56 -1000

    Changed speedometer to pull  linspeed data from rosconfig

--- frontend/src/Components/Speedometer.js
+++ frontend/src/Components/Speedometer.js
@@ -1,26 +1,22 @@
 import React from "react";
 import GaugeChart from 'react-gauge-chart';
 import ROSConfig from '../scripts/ROSConfig';
-import { useState, useEffect } from "react";
-import { init_ros_connection } from '../ROSConnection';
+import { useState, useEffect, useRef } from "react";
 
 
 function Speedometer(props) {
+  let ref = useRef(ROSConfig.linSpeed);
 
-  const cmdVelTopic = new window.ROSLIB.Topic({
-    ros: init_ros_connection.ros,
-    name: init_ros_connection.cmd_vel_topic,
-    messageType: "geometry_msgs/Twist",
-  });
-
-  const [linSpeed, setLinSpeed] = useState(0);
-
+  useEffect(() => {
+    ref.current = ROSConfig.linSpeed;
+  }, [ref.current])
+  
   return(
         <div style={{ backgroundColor: '#282c34' }}>
           <GaugeChart
               id="speedometer-gauge"
               nrOfLevels={20}
-              percent={linSpeed / 100}
+              percent={ref.current / 100}
               animDelay={0.5}
           />
         </div>
