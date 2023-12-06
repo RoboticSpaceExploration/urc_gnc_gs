43340380741a8986f1eefc47e622c7dbfb076140
jalenlum <808jalen@gmail.com>
2023-12-06 13:15:49 -1000

    added button to descrease linSpeed

--- frontend/src/Components/Speedometer.js
+++ frontend/src/Components/Speedometer.js
@@ -1,24 +1,37 @@
-import React from "react";
 import GaugeChart from 'react-gauge-chart';
 import ROSConfig from '../scripts/ROSConfig';
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
 
 function Speedometer(props) {
-  let ref = useRef(ROSConfig.linSpeed);
+
+  const [linSpeed, setLinSpeed] = useState(null)
 
   useEffect(() => {
-    ref.current = ROSConfig.linSpeed;
-  }, [ref.current])
+    setLinSpeed(ROSConfig.linSpeed)
+  }, [])
+
+  const linSpeedUp= () => {
+    let temp = linSpeed * 1.1
+    setLinSpeed(temp)
+  }
+
+  const linSpeedDown= () => {
+    let temp = linSpeed * 0.9
+    setLinSpeed(temp)
+  }
   
   return(
         <div style={{ backgroundColor: '#282c34' }}>
           <GaugeChart
               id="speedometer-gauge"
               nrOfLevels={20}
-              percent={ref.current / 100}
+              percent={linSpeed / 100} //change divisor as needed
               animDelay={0.5}
           />
+          <Button onClick={linSpeedUp}> U </Button>
+          <Button onClick={linSpeedDown}> J </Button>
         </div>
         
   );
