97d0498cd1bedcb153bd2bf9267b37030b3af2fe
pmmanuel315 <pmmanuel315@gmail.com>
2023-12-07 16:00:24 -1000

    waypoint data displayed

--- frontend/src/Components/AutoNav/AutoNavWaypoints.js
+++ frontend/src/Components/AutoNav/AutoNavWaypoints.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import WaypointForm from "../Forms/WaypointForm";
+import WaypointDisplay from '../WaypointDisplay';
 
 export const AutoNavWaypoints = ()=>{
     const [waypointData, setWaypointData] = useState(null);
-    const [queueData, setQueueData] = useState(null);
 
     const cardStyle = { height: "100vh" };
     const titleStyle = { textAlign: "center", marginBottom: "10px" };
@@ -25,31 +25,16 @@ export const AutoNavWaypoints = ()=>{
             console.log(error.response.headers);
             }
         });
-        axios({
-        method: "GET",
-        url: "http://localhost:9000/autonav",
-        })
-        .then((response) => {
-            const res = response.data;
-            setQueueData(res);
-        })
-        .catch((error) => {
-            if (error.response) {
-            console.log(error.response);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            }
-        });
-    }, []);
+    }, [waypointData]);
 
-
+    console.log(waypointData);
     return(
 
         <>
             <div>
                 <h3 style={{ textAlign: "center" }}>Insert Waypoint</h3>
               <WaypointForm/>
-            
+              <WaypointDisplay waypointData={waypointData} />
             </div>
         
         </>
