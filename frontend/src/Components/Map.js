40311c00296b8bf03d0f0236e9923fd5130bbb05
Victor Pagan <vicpagan808@gmail.com>
2024-04-26 17:45:04 -1000

    Update Map.js
    Hoping this works

--- frontend/src/Components/Map.js
+++ frontend/src/Components/Map.js
@@ -1,6 +1,6 @@
-import React, {useState, useCallback, useEffect} from "react";
-import {Button, Form, Container} from "react-bootstrap";
-import {MapContainer, TileLayer, Marker, Polyline} from 'react-leaflet';
+import React, {useState, useEffect} from "react";
+import { Form } from "react-bootstrap";
+import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
 import axios from "axios";
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
@@ -16,16 +16,16 @@ import {init_ros_connection} from "../ROSConnection";
 
 function Map(props) {
 
-    /* dummy data for rover and waypoint location data
+    // dummy data for rover and waypoint location data
     const state = {
         location: [21.2998, -157.8148],
         testLocation: [[21.2992, -157.8148], [21.3000, -157.8155], [21.2995, -157.8159]],
         traceLine: [[21.2998, -157.8148], [21.2992, -157.8148], [21.3000, -157.8155], [21.2995, -157.8159]],
     }
-    */
+
 
     const [roverPosition, setRoverPosition] = useState([21.2984, -157.8168]);
-    const [roverPath, setRoverPath] = useState([]);
+    const [roverPath, setRoverPath] = useState([roverPosition]);
 
     const listener = new window.ROSLIB.Topic({
         ros: init_ros_connection.ros,
@@ -39,7 +39,10 @@ function Map(props) {
             setRoverPosition(newPosition);
             setRoverPath(prevPath => [...prevPath, newPosition]);
             listener.unsubscribe();
+            console.log(roverPath[roverPath.length - 1], newPosition);
+            console.log(roverPath);
         })
+
     }, [roverPosition]);
 
     const mapStyle = {
@@ -67,7 +70,6 @@ function Map(props) {
     });
 
     const [objLocation, setObjLocation] = useState([]);
-    const [lineLocation, setLineLocation] = useState([roverPosition]); //traces the motion of the rover
 
     // allow users to submit waypoints data
     const updateObjLocation = (newLocation) => {
@@ -112,13 +114,13 @@ function Map(props) {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <RoverMarker position={roverPosition} icon={roverIcon} />
-                <Polyline positions={roverPath} color="red" />
                 {/*<ObjMarker position={state.testLocation[2]} icon={objIcon} />*/}
                 {props.waypointData && props.waypointData.map((waypoint, index) => {
                     return (
                         <ObjMarker position={waypoint.position} icon={objIcon} key={index}/>
                     );
                 })}
+                <Polyline positions={roverPath} color="red" />
             </MapContainer>
         </div>
     );
